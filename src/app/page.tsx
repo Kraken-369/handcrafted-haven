8ef876487afa1e123499af11c65f3670d5afc688
Nestor Otondo <notondo@byupathway.edu>
2025-03-26 18:31:13 -0300

    Creating db connection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,21 @@
+'use client';
+
 import Image from "next/image";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch('/api/test');
+      const result = await response.json();
+      setData(result);
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
@@ -49,6 +64,7 @@ export default function Home() {
           >
             Read our docs
           </a>
+          <pre>{JSON.stringify(data, null, 2)}</pre>
         </div>
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">

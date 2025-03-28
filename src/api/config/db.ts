ec2129af3d8b78f04e1f483211f6360b95b81f03
Nestor Otondo <notondo@byupathway.edu>
2025-03-28 18:36:53 -0300

    Adding API test

--- src/api/config/db.ts
+++ src/api/config/db.ts
@@ -1,9 +1,10 @@
 import db, { Connection } from 'mongoose';
 
 const mongoDBURL = process.env.MONGODB_URL;
+const dbName = process.env.DB_NAME;
 
 if (!mongoDBURL) {
-  throw new Error('⚠️ MONGODB_URI it si not defined.');
+  throw new Error('MONGODB_URI it si not defined.');
 }
 
 interface cached {
@@ -24,17 +25,17 @@ const connectDB = async () => {
 
   if (!connect.db.promise) {
     connect.db.promise = db.connect(mongoDBURL, {
-      dbName: process.env.DB_NAME
+      dbName
     }).then(mongoose => mongoose.connection);
   }
 
   try {
     connect.db.conn = await connect.db.promise;
-    console.log('✅ your MongoDB was connected.');
+    console.log(`your MongoDB ${dbName} was connected.`);
 
     return connect.db.conn;
   } catch (error) {
-    console.error('❌ Error: Can not connected to MongoDB:', error);
+    console.error('Error: Can not connected to MongoDB:', error);
     
     throw error;
   }

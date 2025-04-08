2c3ff9a8bb0539bddfa27a2fa0da70ccce776598
Nestor Otondo <notondo@byupathway.edu>
2025-04-08 16:19:09 -0300

    Adding domains permisions to load images

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['fastly.picsum.photos'],
+    domains: [
+      'fastly.picsum.photos',
+      'cdn.pixabay.com'
+    ],
   },
 };
 

0a08456746ec470bd807a0fe2972d70a5a4cfbea
Nestor Otondo <notondo@byupathway.edu>
2025-04-10 20:32:16 -0300

    Adding end point to get user by ID

--- src/api/controllers/user.ts
+++ src/api/controllers/user.ts
@@ -1,4 +1,5 @@
 import { NextApiRequest, NextApiResponse } from 'next';
+import { ObjectId } from 'mongodb';
 import bcrypt from 'bcryptjs';
 import connectDB from '@/api/config/db';
 import User from '@/api/models/user';
@@ -40,6 +41,31 @@ export const getAllUsers = async (req: NextApiRequest, res: NextApiResponse) => 
     const users = await User.find().select('-password');
     
     return res.status(200).json(users);
+  } catch (error) {
+    return res.status(500).json({ message: `Internal Server Error: ${error}` });
+  }
+};
+
+export const getUserById = async (req: NextApiRequest, res: NextApiResponse) => {
+  const { id } = req.query;
+  const userId = Array.isArray(id) ? id[0] : id;
+
+  if (!userId) {
+    return res.status(400).json({ message: 'Category ID is required.' });
+  }
+
+  if (!ObjectId.isValid(userId)) {
+    return res.status(400).json({ message: 'Invalid category ID format.'});
+  }
+
+  try {
+    const user = await User.findById(userId).select('-password');
+
+    if (!user) {
+      return res.status(404).json({ message: 'User not found.' });
+    }
+
+    return res.status(200).json(user);
   } catch (error) {
     return res.status(500).json({ message: `Internal Server Error: ${error}` });
   }
